6358480032f8bddf1d1e5ce93c64b22519555c31
denisenko_ilya <07.iden@gmail.com>
2019-11-22 21:32:08 +0300

    почтовые настройки вынесены в конфиг

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// noinspection SpellCheckingInspection
 exports.config = {
     watchScoreSeqCount: 3,
     watchScoreSeq: ['1:1', '2:2', '0:2', '0:3', '0:4'],
@@ -8,5 +9,15 @@ exports.config = {
     cachedGamesSize: 30,
     useDummyUrl: false,
     fileWritingEnabled: true,
+
+    mail: {
+        host: 'smtp.yandex.ru',
+        port: 465,
+        user: 'lshtfum.ashf',
+        pass: 'ololo&trololo',
+        subject: 'Fonbet',
+        recipients: 'ilya0191@ya.ru',
+        from: 'ilya0191 <lshtfum.ashf@ya.ru>',
+    }
 };
 
--- src/notifier.js
+++ src/notifier.js
@@ -1,40 +1,27 @@
 'use strict';
+const config = require("./config.js");
+const mail = config.config.mail;
 const nodemailer = require('nodemailer');
-const recipients = 'ilya0191@ya.ru';
 
 exports.sendMail = async function(message, test) {
-
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = {
-        user: 'lshtfum.ashf',
-        pass: 'ololo&trololo',
-    };
-
-    // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'smtp.yandex.ru',
-        port: 465,
-
-
+        host: mail.host,
+        port: mail.port,
         secure: true, // true for 465, false for other ports
         auth: {
-            user: testAccount.user, // generated ethereal user
-            pass: testAccount.pass, // generated ethereal password
+            user: mail.user,
+            pass: mail.pass,
         }
     });
-
-
     if (test) {
         await transporter.verify();
         return;
     }
-    // send mail with defined transport object
     return await transporter.sendMail({
-        from: 'ilya0191 <lshtfum.ashf@ya.ru>', // sender address
-        to: recipients, // list of receivers
-        subject: 'Привет! ', // Subject line
-        text: message, // plain text body
+        from: mail.sender,
+        to: mail.recipients,
+        subject: mail.subject,
+        text: message,
     });
 
 
